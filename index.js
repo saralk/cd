1bc6e4efb80e1cf111f82184a5b2454d325c0c9a
Saral Kaushik <saral.kaushik@vincuventas.com>
2018-09-30 20:23:29 +0100

    refactor code

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const fs = require('fs');
 const express = require('express');
 const rp = require('request-promise-native');
 
-var app = express();
-app.listen(process.env.PORT || 3000);
+const app = express();
+const port = process.env.PORT || 3000 
+
+if (!API_KEY) {
+    throw new Error('No API_KEY environment variable set');
+}
+
+app.listen(port);
+console.log(`Listening on port ${port}`);
 
 //build the data
 const priorities = JSON.parse(fs.readFileSync('data/data.json')).reduce((map, obj) => {
     map[obj['District'].toLowerCase()] = {
         category: obj['Category'],
         priority: obj['Priority Number']
-    }
+   }
 
     return map;
 }, {});
@@ -27,7 +34,8 @@ function addLeadingZero(num) {
 }
 
 async function getPriorityForCoordinates(lat, lon) {
-    let url = 'https://www.googleapis.com/civicinfo/v2/representatives?key=' + API_KEY + '&address=' + lat + ',' + lon + '&includeOffices=false';
+    let url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${API_KEY}&address=${lat},${lon}&includeOffices=false`;
+    console.log(url);
     return rp(url).then((response) => {
         return new Promise((resolve, reject) => {
             let r = JSON.parse(response);
@@ -44,7 +52,6 @@ async function getPriorityForCoordinates(lat, lon) {
 }
 
 app.get('/priorities', async (req, res) => {
-    console.log(req.query);
     let home = await getPriorityForCoordinates(...req.query.home.split(','));
     let uni = await getPriorityForCoordinates(...req.query.uni.split(','));
 
@@ -57,3 +64,5 @@ app.get('/priorities', async (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use('/static', express.static('static'));
---
+++ static/script.js
@@ -0,0 +1,78 @@
+var home, uni
+
+function initMap() {
+    var options = {
+        componentRestrictions: {
+            country: "us"
+        }
+    };
+
+    home = new google.maps.places.SearchBox(document.getElementById('homeinput'), options);
+    uni = new google.maps.places.SearchBox(document.getElementById('uniinput'), options);
+
+    google.maps.event.addListener(home, 'places_changed', handleChange);
+    google.maps.event.addListener(uni, 'places_changed', handleChange);
+}
+
+function handleChange() {
+    var home_place = home.getPlaces();
+    var uni_place = uni.getPlaces();
+
+    var results_el = document.getElementById("results");
+
+    if (home_place && uni_place) {
+        var home_coords = home_place[0].geometry.location;
+        var uni_coords = uni_place[0].geometry.location;
+
+        getPriorities(home_coords, uni_coords, function(response) {
+            var priorities = JSON.parse(response);
+
+            renderResults(priorities);
+        });
+    }
+}
+
+function getPriorities(home_c, uni_c, cb) {
+    let home_coords = home_c.lat() + ',' + home_c.lng();
+    let uni_coords = uni_c.lat() + ',' + uni_c.lng();
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            cb(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", '/priorities?home=' + encodeURIComponent(home_coords) + '&uni=' + encodeURIComponent(uni_coords), true);
+    xmlHttp.send();
+}
+
+function renderResults(results) {
+    "use strict";
+
+    Array.from(document.getElementsByClassName('home-name')).forEach((el) => {
+        el.innerHTML = results.home.district_name.name;
+    });
+
+    Array.from(document.getElementsByClassName('school-name')).forEach((el) => {
+        el.innerHTML = results.uni.district_name.name;
+    });
+
+    const page = document.getElementById('app');
+
+    ['home-page', 'result-home', 'result-school', 'result-tossup'].forEach((className) => {
+        page.classList.remove(className);
+    });
+    page.classList.add('result-page');
+
+    let className;
+
+    if (results.home.priority > results.uni.priority) {
+        className = 'result-home';
+    }
+    if (results.uni.priority > results.home.priority) {
+        className = 'result-school';
+    }
+    if (results.uni.priority == results.home.priority) {
+        className = 'result-tossup';
+    }
+
+    page.classList.add(className);
+}
